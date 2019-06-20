c97321b1447bb3f555f5af4be8596cd75ef70bfc
tinkrbay <43562548+tinkrbay@users.noreply.github.com>
2019-06-20 13:09:07 +0530

    Add files via upload

--- pb-payee-api-uservice/server.js
+++ pb-payee-api-uservice/server.js
@@ -2,10 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  UserDetails = require('./api/models/User'),
   PayeeDetails = require('./api/models/Payee'),
-  BalanceDetails = require('./api/models/Balance'),
-  TransferDetails = require('./api/models/Transfer'),  
   bodyParser = require('body-parser'),
   path = require('path'),
   cors = require('cors');
@@ -24,12 +21,6 @@ app.use(bodyParser.json());
 
 var routes = require('./api/routes/PayeeRoutes');
 routes(app);
-routes1 = require('./api/routes/UserRoutes');
-routes1(app);
-routes2 = require('./api/routes/BalanceRoutes');
-routes2(app);
-routes3 = require('./api/routes/TransferRoutes');
-routes3(app);
 app.listen(port);
 
 
--- pb-transfer-api-uservice/server.js
+++ pb-transfer-api-uservice/server.js
@@ -2,10 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  UserDetails = require('./api/models/User'),
-  PayeeDetails = require('./api/models/Payee'),
-  BalanceDetails = require('./api/models/Balance'),
-  TransferDetails = require('./api/models/Transfer'),  
+  BalanceDetails = require('./api/models/Transfer'),
   bodyParser = require('body-parser'),
   path = require('path'),
   cors = require('cors');
@@ -22,14 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/PayeeRoutes');
+var routes = require('./api/routes/TransferRoutes');
 routes(app);
-routes1 = require('./api/routes/UserRoutes');
-routes1(app);
-routes2 = require('./api/routes/BalanceRoutes');
-routes2(app);
-routes3 = require('./api/routes/TransferRoutes');
-routes3(app);
 app.listen(port);
 
 
